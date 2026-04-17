6d1f1c0b4476ba5c2636bb7cca0dce5d78023186
danielgarciadasilva <daniel.garciadasilva3@gmail.com>
2026-04-17 19:39:23 -0300

    Teste

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -1,5 +1,9 @@
 public class OlaMundo {
     public static void main(String[] args) {
         System.out.println("Olá Mundo!");
+
+        for (int i = 0; i < 100; i++) {
+            System.out.println("testes");
+        }
     }
 }
